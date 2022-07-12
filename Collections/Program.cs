29c8a9ac1f045a089d7cc8dab51f4be87479229c
javidancode <javidanig@code.edu.az>
2022-07-12 04:46:56 +0400

    Added Collections table

---
+++ Collections/Program.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Dersde yazdiglarimiz
+
+            //List<int> nums = new List<int>();
+
+            //nums.Add(15);
+            //nums.Add(78);
+            //nums.Add(12);
+            //nums.Add(99);
+
+            //List<string> words = new List<string>();
+
+            //words.Add("cavidan");
+            //words.Add("javidan");
+            //words.Add("coni");
+            //words.Add("joni");
+
+            //foreach (var item in words)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //foreach (var item in nums)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //for (int i = 0; i < nums.Count; i++)
+            //{
+            //    Console.WriteLine(nums[i]);
+            //}
+
+            //if (nums.Count == 15)
+            //{
+            //    Console.WriteLine("List count is [0]",nums.Count);
+            //}
+
+            //List<Person> people = new List<Person>();
+            //Person person1 = new Person();
+            //person1.Id = 5;
+            //person1.Name = "Coni";
+            //person1.Email = "Coni@gmail.com";
+
+            //Person person2 = new Person
+            //{
+            //    Id = 6,
+            //    Name = "ferda",
+            //    Email = "Ferda@gmail.com"
+
+            //};
+
+            //people.Add(person2);
+
+            ////people.Add(person1);
+
+            //people.Add(new Person { Id = 1, Name = "Cavidan", Email = "Cavidan@gmail.com" });
+            //people.Add(new Person { Id = 2, Name = "Eli", Email = "Eli@gmail.com" });
+            //people.Add(new Person { Id = 3, Name = "Cavad", Email = "Cavad@gmail.com" });
+            //people.Add(new Person { Id = 4, Name = "Samir", Email = "Samir@gmail.com" });
+
+            ////GetStudents(people);
+
+            //var result = GetPersonByName(people, "Cavidan");
+            //Console.WriteLine(result.Name + "-" + result.Id + "-" + result.Email);
+
+
+
+
+
+
+
+
+            //Hashtable Datas = new Hashtable();
+
+            //Datas.Add(1, "Cavidan");
+            //Datas.Add(2, "Coni");
+
+            //foreach (DictionaryEntry item in Datas.Keys)
+            //{
+            //    Console.WriteLine(item.Key + "-" + item.Value);
+            //}
+
+
+            //SortedList Datas = new SortedList();
+
+            //Datas.Add(1, "Cavidan");
+            //Datas.Add(2, "Coni");
+
+            //foreach (DictionaryEntry item in Datas.Keys)
+            //{
+            //    Console.WriteLine(item.Key + "-" + item.Value);
+            //}
+
+
+
+            //SortedList<int, string> datas = new SortedList<int, string>();
+
+            //datas.Add(1, "Cavidan");
+            //datas.Add(2, "Coni");
+
+            //foreach (KeyValuePair<int,string> item in datas)
+            //{
+            //    Console.WriteLine(item.Key + "-" + item.Value);
+            //}
+
+
+
+            //ArrayList datas = new ArrayList();
+
+            //datas.Add("Cavidan");
+            //datas.Add(18);
+            //datas.Add(true);
+
+            //foreach (var item in datas)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            //HashSet<int> datas = new HashSet<int>();
+
+            //datas.Add(9);
+            //datas.Add(9);
+            //datas.Add(3);
+
+            //foreach (var item in datas)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+
+            //Stack<int> datas = new Stack<int>();
+
+            //datas.Push(17);
+            //datas.Push(19);
+            //datas.Push(25);
+
+            //foreach (var item in datas)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            //Queue<int> datas = new Queue<int>();
+
+            //datas.Enqueue(5);
+            //datas.Enqueue(6);
+            //datas.Enqueue(7);
+
+            //foreach (var item in datas)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            //Dictionary<string, string> datas = new Dictionary<string, string>();
+
+            //datas.Add("Admin", "Cavidan");
+            //datas.Add("Member", "Eli");
+            //datas.Add("Superadmin", "Cavad");
+
+
+            //foreach (KeyValuePair<string,string> item in datas)
+            //{
+            //    if (item.Key == "Member")
+            //    {
+            //        Console.WriteLine($"Name : {item.Value}, Role : {item.Key}");
+            //        break;
+            //    }
+            //}
+        }
+
+        //public static void GetStudents(List<Person> students)
+        //{
+        //    foreach (var stu in students)
+        //    {
+        //        Console.WriteLine("students: id - {0} , Name - {1} , Email - {2}" , stu.Id , stu.Name , stu.Email);
+        //    }
+        //}
+
+        //public static Person GetPersonByName(List<Person> people, string name)
+        //{
+        //    var person = people.Find(m => m.Name == name);
+        //    return person;
+        //}
+    }
+
+    //public class Person
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //    public string Email { get; set; }
+    //}
+
+    #endregion Dersde yazdiglarimiz
+}
